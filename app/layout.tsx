5eae426a9c9ac0c1fc68e36f6315f73b3de4ce17
Nicolas Sicolo <sicolo2208@gmail.com>
2024-05-20 18:49:05 -0300

    First commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,20 @@
+//Justamente layout es lo que envuelve nuestras paginas. Envuelve a nuestro page.tsx.
+//Layout.tsx envuelve toda nuestra aplicacion
+
+import './ui/global.css';
+
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
+
   return (
     <html lang="en">
-      <body>{children}</body>
+      {/**Children justamente es nuestro page.tsx */}
+      <body>{children}</body> 
     </html>
   );
 }
