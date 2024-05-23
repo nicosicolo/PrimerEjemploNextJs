038be6e09060cde05c6714b9fb6ce63fdc9a1d4b
Nicolas Sicolo <sicolo2208@gmail.com>
2024-05-23 00:17:46 -0300

    Cambios MBAM2

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,6 +3,16 @@
 import { fetchRevenue } from "../lib/data"
 
 export default async function DashboardPage() {
+    //Hacer fetching del lado del cliente representa una perdida de tiempo.
+    //Es mejor que nuestro servidor haga el fetching de datos, hagas las transformaciones
+    //requeridas y luego descargar el html directamente asi se renderiza desde el lado del cliente
+    //Entonces, trabaja en el servidor y devolvele al cliente ya todo listo para ser renderizado.
+ 
+    // Ejemplo de como hacer fetching de datos desde un server component
+    // const ejemploAPi = await fetch('https://api.example.com')
+    // const json = await ejemploApi.json()
+    // console.log(json);
+    
     const revenue = await fetchRevenue();
     console.log(revenue);
     
