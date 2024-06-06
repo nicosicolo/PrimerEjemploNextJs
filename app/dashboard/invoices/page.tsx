4e7c87555a35a32023b048070d6e36c91ee11fb8
Nicolas Sicolo <nsicolo@tecnoaccion.com.ar>
2024-06-06 12:31:30 -0300

    cambio thinkpad

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,6 +5,12 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+
+
+/* Esta Page cuenta con los componentes Search y Table.
+En Search el usuario realiza el input del customer que necesite buscar.
+El nombre del customer se pasa a traves de parámetros en la url.
+ */
  
 export default async function Page({
   searchParams
